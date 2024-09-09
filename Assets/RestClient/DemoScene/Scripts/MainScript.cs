94de4d5daf1db87fce541c6f8dac94e1809f8c7c
emiliagit <94327724+emiliagit@users.noreply.github.com>
2024-09-09 15:14:26 -0300

    last commit

--- Assets/RestClient/DemoScene/Scripts/MainScript.cs
+++ Assets/RestClient/DemoScene/Scripts/MainScript.cs
@@ -23,7 +23,7 @@ public class MainScript : MonoBehaviour {
 	public void Get(){
 
 		// We can add default request headers for all requests
-		RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
+		//RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
 
 		RequestHelper requestOptions = null;
 
